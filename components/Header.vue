95d123444da40ff9b77dcacb14afef4ffd9d1e28
shujinarazaki <shujinarazaki@protonmail.com>
2019-06-18 11:34:33 +0900

    initialize

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    <nuxt-link to="/">Hello Qiita with Nuxt.js</nuxt-link>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped></style>
